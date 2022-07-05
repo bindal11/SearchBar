1c8f939830132f980da631d308420f63ea007bc4
Bhavika <brbindal@tekion.com>
2022-07-05 19:41:57 +0530

    new changes

---
+++ script.js
@@ -0,0 +1,18 @@
+
+//get all required elements
+const searchWrapper = document.querySelector(".search-input");
+const inputBox = searchWrapper.querySelector('input');
+const suggBox = searchWrapper.querySelector('.autocom-box')
+const icon = searchWrapper.querySelector('.icon');
+let a = searchWrapper.querySelector('a');
+let webLink;
+
+inputBox.onkeyup = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if(userData) {
+    
+  }
+}
+
+
